f85c7d52b775a9b482b4c4eba26e22580177b844
SharkiPro <49125292+SharkiPro@users.noreply.github.com>
2020-04-07 23:46:59 +0200

    ✔ Stable V1.2
    ✨ Add possibility to change color

--- create.php
+++ create.php
@@ -8,33 +8,56 @@
     <title>Sharkloading</title>
 </head>
 <body>
-<h3 class="mx-auto" style="width: 200px;">SharkLoading</h3>
+<h3 class="text-white text-md-left font-italic badge badge-dark text-wrap">By Sharki (Discord: Sharki#6540)</h3>
+<h3 class="mx-auto text-center text-white">SharkLoading</h3>
 &nbsp
-<form class="mx-auto" style="width: 200px;" action="" method="post">
+<form class="mx-auto" style="width: 200px;" method="post">
   <div class="form-group">
-    <label>Nom du serveur: (Obligatoire)</label>
+    <label class="text-white">Couleur principal:</label>
+    <select class="form-control" name="couleur">
+      <option value="text-black">Noir</option>
+      <option value="text-white">Blanc</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label class="text-white">Nom du serveur: (Obligatoire)</label>
     <input type="text" class="form-control" name="subject1" id="subject1" placeholder="Exemple: Sharkland">
   </div>
   <div class="form-group">
-    <label>Description du serveur: (facultatif)</label>
+    <label class="text-white">Description du serveur: (facultatif)</label>
     <input type="text" class="form-control" name="subject2" id="subject2" placeholder="Exemple: Rien à dire">
   </div>
   <div class="form-group">
-    <label>Logo du serveur: (facultatif)</label>
+    <label class="text-white">Logo du serveur: (facultatif)</label>
     <input type="text" class="form-control" name="logo" id="logo" placeholder="Exemple: https://image.com/image.jpg">
   </div>
   <div class="form-group">
-    <label>Arriere plan: (Obligatoire)</label>
+    <label class="text-white">Arriere plan: (Obligatoire)</label>
     <input type="text" class="form-control" name="background" id="background" placeholder="Exemple: https://image.com/image.jpg">
   </div>
   <button type="submit" class="btn btn-success" name="ok">Accepter</button>
 </form>
+
+<style>
+            body {
+              background-image: url("https://i.pinimg.com/originals/17/b5/b3/17b5b32cb87471940a5ee8e1aecff557.jpg");
+              background-repeat: no-repeat;
+              background-attachment: fixed;
+              background-size: cover;
+            }
+</style>
+
 <?php
 
 $logo = !empty($_POST['logo'])?$_POST['logo']:null;
 $servername = !empty($_POST['subject1'])?$_POST['subject1']:null;
 $desc = !empty($_POST['subject2'])?$_POST['subject2']:null;
 $background = !empty($_POST['background'])?$_POST['background']:null;
+$couleur = !empty($_POST['couleur'])?$_POST['couleur']:"text-black";
+$couleurbg = '#000';
+if($couleur == 'text-white'){
+  $couleurbg = '#fff';
+}
 
 if(isset($_POST['ok'])){
     if($servername && $background){
@@ -51,11 +74,11 @@ if(isset($_POST['ok'])){
             <body>
             <div class='mx-auto'>
             <img class='mx-auto' style='width: 200px;' src={$logo}>
-            <h3 class='mx-auto text-center text-black' style='font-size:50px;'>{$servername}</h3>
-            <h3 class='mx-auto text-center text-black' style='font-size:20px;'>{$desc}</h3>
+            <h3 class='mx-auto text-center {$couleur}' style='font-size:50px;'>{$servername}</h3>
+            <h3 class='mx-auto text-center {$couleur}' style='font-size:20px;'>{$desc}</h3>
             </div>
 
-            <div class='mx-auto' style='width: 625px;' id='outerContainer'>
+            <div class='mx-auto' style='width: 600px;' id='outerContainer'>
               <div id='container'>
                 <div class='circle' style='animation-delay: 0s'></div>
                 <div class='circle' style='animation-delay: 1s'></div>
@@ -84,7 +107,7 @@ if(isset($_POST['ok'])){
          
             .circle {
               border-radius: 50%;
-              background-color: black;
+              background-color: {$couleurbg};
               width: 150px;
               height: 150px;
               position: absolute;
